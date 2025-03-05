77ca50dce5ab0e7a9f37ddf27ccfe536d1530872
Thomas Körner <t.koerner@heimrich-hannot.de>
2025-03-05 17:52:45 +0100

    add some english locales

---
+++ contao/languages/en/tl_settings.php
@@ -0,0 +1,13 @@
+<?php
+
+$arrLang = &$GLOBALS['TL_LANG']['tl_settings'];
+
+/**
+ * Fields
+ */
+$arrLang['cleanerOrder'] = ['Order', 'Here you can define the order of the cleaner.'];
+
+/**
+ * Legends
+ */
+$arrLang['cleaner_legend'] = 'Cleaner';
